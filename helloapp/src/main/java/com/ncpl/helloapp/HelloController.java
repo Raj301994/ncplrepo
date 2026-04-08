74e05c18b51c8f02958fbcb1ef434c942464d435
Raj301994 <rajtestcan@gmail.com>
2026-04-08 14:31:40 -0400

    add hello app folder

---
+++ helloapp/src/main/java/com/ncpl/helloapp/HelloController.java
@@ -0,0 +1,17 @@
+package com.ncpl.helloapp;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "!!Welcome to springboot project!!";
+        
+    }
+    
+    
+}
